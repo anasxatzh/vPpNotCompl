f7a4c7dc5488f65a1da35bdf43f59a4d27b4d5e2
anasxatzh <105356542+anasxatzh@users.noreply.github.com>
2022-11-23 22:57:37 +0200

    Update eStore.py

--- eStore.py
+++ eStore.py
@@ -1,22 +1,10 @@
 
-from pPa_Lib.environment import Environment
-from pPa_Lib.user_profile import UserProfile
-from pPa_Lib.electrical_energy_storage import ElectricalEnergyStorage
-from pPa_Lib.photovoltaic import Photovoltaic
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# env
-start = "2021-06-01 00:00:00"
-end = "2021-06-07 23:45:00"
-year = "2021"
 
-# uProfile
-latitude = 50.941357
-longitude = 6.958307
 
-# PV
 unit = "kW"
 name = "bus"
 module_lib = "SandiaMod"
@@ -29,21 +17,18 @@ modules_per_string = 4
 strings_per_inverter = 2
 temp_lib = 'sapm'
 temp_model = 'open_rack_glass_glass'
-# Storage!
 timebase = 15
 charge_efficiency = 0.98
 discharge_efficiency = 0.98
 max_power = 4  
 capacity = 4  
 max_c = 1  
-
-# Test!
 timestamp_int = 48
 timestamp_str = "2021-06-01 12:00:00"
 
 
 environment = Environment(timebase=timebase, start=start, end=end, year=year)
-environment.get_pv_data(file="./input/pv/dwd_pv_data_2015.csv")
+environment.get_pv_data(file=".csv")
 
 user_profile = UserProfile(
     identifier=name, latitude=latitude, longitude=longitude
@@ -95,7 +80,7 @@ house_loadshape["residualLoad"] = (
 storage.residual_load = house_loadshape.residual_load
 
 
-def test_prepare_time_series(storage):
+def (storage):
 
     storage.prepare_time_series()
     print("TimeSeries:")
@@ -104,20 +89,20 @@ def test_prepare_time_series(storage):
     plt.show()
 
 
-def test_value_for_timestamp(storage, timestamp):
+def (storage, timestamp):
 
     timestepvalue = storage.value_for_timestamp(timestamp)
     print("\nValForTimeStamp:\n", timestepvalue)
 
 
-def test_observationsForTimestamp(storage, timestamp):
+def (storage, timestamp):
 
     print("ObsForTimeStamp:")
     observation = storage.observations_for_timestamp(timestamp)
     print(observation, "\n")
 
 
-def test_operate_storage(storage, timestamp):
+def (storage, timestamp):
 
     print("opStorage:")
     state_of_charge, res_load = storage.operate_storage(
@@ -126,12 +111,3 @@ def test_operate_storage(storage, timestamp):
     print("SOS: ", state_of_charge)
     print("residualLoad: ", res_load)
 
-
-test_prepare_time_series(storage)
-test_value_for_timestamp(storage, timestamp_int)
-test_value_for_timestamp(storage, timestamp_str)
-
-test_observationsForTimestamp(storage, timestamp_int)
-test_observationsForTimestamp(storage, timestamp_str)
-
-test_operate_storage(storage, timestamp_str)
